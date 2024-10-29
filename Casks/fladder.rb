28faf5ba7da40d975f60ed655270c69bef475d5c
Ben McLean <benmclean007@gmail.com>
2024-10-29 21:43:25 +1100

    Create fladder.rb

---
+++ Casks/fladder.rb
@@ -0,0 +1,20 @@
+cask "fladder" do
+  version "0.3.1"
+  sha256 "a75ff34c39973a2417db06ccd179846a8dea6adfc85b65692eff3a276d904827"
+
+  url "https://github.com/DonutWare/Fladder/releases/download/v#{version}/Fladder-macOS-#{version}.dmg"
+  name "fladder"
+  desc "Simple Jellyfin Frontend built on top of Flutter"
+  homepage "https://github.com/DonutWare/Fladder"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :mojave"
+
+  app "Fladder.app"
+
+  zap trash: "~/Library/Containers/Fladder"
+end
